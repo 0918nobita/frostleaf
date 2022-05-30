338691de958c38b49a3ae92aac08f777eed9b563
0918nobita <nobita.0918@gmail.com>
2022-05-30 10:22:26 +0900

    :sparkles: StaticComponent から HTML 文字列への変換を実装した

--- packages/example/src/main.ts
+++ packages/example/src/main.ts
@@ -1,17 +1,72 @@
+type Attrs = Record<string, string>;
+
 /** 状態遷移しないコンポーネント */
 type StaticComponent =
     | string
     | {
           tag: string;
-          attrs: Record<string, unknown>;
+          attrs: Attrs;
           children: StaticComponent[];
       };
 
 const h = (
     tag: string,
-    attrs: Record<string, unknown>,
+    attrs: Attrs,
     children: StaticComponent[]
 ): StaticComponent => ({ tag, attrs, children });
+
+const html: StaticComponent = h("html", {}, [
+    h("head", {}, [
+        h("meta", { charset: "utf-8" }, []),
+        h("title", {}, ["Generated Page"]),
+    ]),
+    h("body", {}, []),
+]);
+
+const voidElements: readonly string[] = [
+    "area",
+    "base",
+    "br",
+    "col",
+    "command",
+    "embed",
+    "hr",
+    "img",
+    "input",
+    "keygen",
+    "link",
+    "meta",
+    "param",
+    "source",
+    "track",
+    "wbr",
+];
+
+const renderStaticComponent = (staticComponent: StaticComponent): string => {
+    if (typeof staticComponent === "string") return staticComponent;
+
+    const { tag, attrs, children } = staticComponent;
+
+    const attrFragments = Object.keys(attrs).map(
+        (key) => `${key}="${attrs[key]}"`
+    );
+    const renderedAttrs =
+        attrFragments.length != 0 ? ` ${attrFragments.join(" ")}` : "";
+
+    if (voidElements.includes(tag)) {
+        if (children.length != 0)
+            throw new Error("Children of void element must be empty");
+        return `<${tag}${renderedAttrs}>`;
+    }
+
+    const renderedChildren = (
+        children.length != 0 ? children.map(renderStaticComponent) : []
+    ).join("");
+
+    return `<${tag}${renderedAttrs}>${renderedChildren}</${tag}>`;
+};
+
+console.log(renderStaticComponent(html));
 
 /** ブラウザ側で実行する JS のパス */
 type ScriptPath = string;
@@ -24,16 +79,6 @@ type ScriptPath = string;
 type ComponentWithEffect<Props> = (
     props: Props
 ) => Promise<[StaticComponent, ScriptPath[]]>;
-
-const html: StaticComponent = h("html", {}, [
-    h("head", {}, [
-        h("meta", { charset: "utf-8" }, []),
-        h("title", {}, ["Generated Page"]),
-    ]),
-    h("body", {}, []),
-]);
-
-console.dir(html, { depth: null });
 
 const dynamic: ComponentWithEffect<string> = async (content: string) => {
     return [h("p", {}, [`${content}!`]), ["foo.client.ts"]];
