d7ba20c3baef26892b6e311b9ac7ebc588010403
0918nobita <nobita.0918@gmail.com>
2022-06-01 02:14:53 +0900

    :wrench: ES Modules を使うように設定した

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,1 +1,8 @@
-console.log("Frostleaf CLI");
+import chalk from "chalk";
+import { render } from "@frostleaf/build-time";
+
+console.log(chalk.bold.green("Frostleaf CLI"));
+
+// const indexPagePath = "../../example/dist/pages/index.js";
+// console.log("Result:")
+// import(indexPagePath).then((mod) => console.log(mod.IndexPage));
--- packages/example/build.ts
+++
@@ -1,6 +0,0 @@
-import { render } from "@frostleaf/build-time";
-
-import { IndexPage } from "./pages/index";
-
-console.log("Generating pages...");
-render(IndexPage).then(console.log);
