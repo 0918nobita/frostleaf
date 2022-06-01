52f00a8b9a57f491de4bfc6a657c5732f2c4f84d
0918nobita <nobita.0918@gmail.com>
2022-06-01 16:58:12 +0900

    :art: Format code

--- packages/example/pages/dyn/[name].ts
+++ packages/example/pages/dyn/[name].ts
@@ -3,10 +3,8 @@ import { Fragment, Page, h } from "@frostleaf/build-time";
 import Hello from "../../components/Hello";
 import World from "../../components/World";
 
-export const DynamicPage: Page = ({ query }) => h(Fragment, {}, [
-    h(Hello, {}, []),
-    h(World, { name: query.name! }, []),
-]);
+export const DynamicPage: Page = ({ query }) =>
+    h(Fragment, {}, [h(Hello, {}, []), h(World, { name: query.name! }, [])]);
 
 export const getPaths = () => ({ paths: ["foo", "bar"] });
 
--- packages/example/pages/index.ts
+++ packages/example/pages/index.ts
@@ -3,9 +3,7 @@ import { Fragment, Page, h } from "@frostleaf/build-time";
 import Hello from "../components/Hello";
 import World from "../components/World";
 
-const IndexPage: Page = () => h(Fragment, {}, [
-    h(Hello, {}, []),
-    h(World, { name: "world" }, []),
-]);
+const IndexPage: Page = () =>
+    h(Fragment, {}, [h(Hello, {}, []), h(World, { name: "world" }, [])]);
 
 export default IndexPage;
