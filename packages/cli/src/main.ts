5682040e36ffc8d7a1d111d2ffd8178d8409b2dd
0918nobita <nobita.0918@gmail.com>
2022-06-01 17:36:31 +0900

    :sparkles: components ディレクトリ内のファイルを読み込むようにした

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,5 +1,13 @@
 import arg from "arg";
 import chalk from "chalk";
+import type { Stats } from "fs";
+import fs from "fs/promises";
+import path from "path";
+
+const printAndExit = (msg: string, code = 1) => {
+    console.error(msg);
+    process.exit(code);
+};
 
 const args = arg(
     {
@@ -9,12 +17,40 @@ const args = arg(
     { argv: process.argv }
 );
 
-if (args["--help"]) {
-    console.log(chalk.bold.magenta("Frostleaf CLI"));
-    process.exit(0);
-}
+if (args["--help"]) printAndExit(chalk.bold.magenta("Frostleaf CLI"), 0);
 
-if (args["--version"]) {
-    console.log("0.1.0");
-    process.exit(0);
-}
+if (args["--version"]) printAndExit("0.1.0", 0);
+
+const projectPath =
+    args._.length >= 3 ? path.resolve(args._[1]) : process.cwd();
+
+const getComponentPaths = async (): Promise<string[]> => {
+    const componentsDir = path.join(projectPath, "components");
+    let stat: Stats;
+    try {
+        stat = await fs.stat(componentsDir);
+    } catch (e) {
+        return [];
+    }
+    if (!stat.isDirectory())
+        throw new Error(`components directory must be folder`);
+    try {
+        return (await fs.readdir(componentsDir)).map((p) =>
+            path.join(componentsDir, p)
+        );
+    } catch (e) {
+        throw new Error("Failed to read components directory");
+    }
+};
+
+(async () => {
+    try {
+        const componentPaths = await getComponentPaths();
+        for (const componentPath of componentPaths) {
+            console.log(`[${componentPath}]`);
+            console.log((await fs.readFile(componentPath)).toString());
+        }
+    } catch (e: unknown) {
+        printAndExit(String(e));
+    }
+})();
