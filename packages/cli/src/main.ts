69c4d389b0b695a0412c14a9f9228af4cf285044
0918nobita <nobita.0918@gmail.com>
2022-06-02 12:22:28 +0900

    :sparkles: コンパイルして得られた JS を /_frostleaf 内に書き出すようにした

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -28,19 +28,22 @@ const getComponentPaths = async (componentsDir: string): Promise<string[]> => {
     if (!stat.isDirectory())
         throw new Error(`components directory must be folder`);
     try {
-        return (await fs.readdir(componentsDir)).map((p) =>
-            path.join(componentsDir, p)
-        ).filter((p) => p.endsWith(".ts"));
+        return (await fs.readdir(componentsDir))
+            .map((p) => path.join(componentsDir, p))
+            .filter((p) => p.endsWith(".ts"));
     } catch (e) {
         throw new Error("Failed to read components directory");
     }
 };
 
-const parseComponentDefs = async (
-    componentsDir: string,
+const compileComponentDefs = async (
+    destDir: string,
     componentPaths: string[]
 ) => {
     try {
+        const destComponentsDir = path.join(destDir, "components");
+        await fs.mkdir(destComponentsDir, { recursive: true });
+
         for (const componentPath of componentPaths) {
             console.log(`Compiling ${componentPath} ...`);
             const mod = await swc.parseFile(componentPath, {
@@ -48,7 +51,7 @@ const parseComponentDefs = async (
             });
             const output = await swc.transform(mod);
             const outFilePath = path.join(
-                componentsDir,
+                destComponentsDir,
                 `${path.parse(componentPath).name}.js`
             );
             await fs.writeFile(outFilePath, output.code);
@@ -60,12 +63,20 @@ const parseComponentDefs = async (
 
 const main = async () => {
     if (args["--help"]) printAndExit(chalk.bold.magenta("Frostleaf CLI"), 0);
+
     if (args["--version"]) printAndExit("0.1.0", 0);
-    const projectPath =
+
+    const projectRoot =
         args._.length >= 3 ? path.resolve(args._[1]) : process.cwd();
-    const componentsDir = path.join(projectPath, "components");
+
+    const componentsDir = path.join(projectRoot, "components");
+
     const componentPaths = await getComponentPaths(componentsDir);
-    await parseComponentDefs(componentsDir, componentPaths);
+
+    const frostleafDir = path.join(projectRoot, "_frostleaf");
+    await fs.mkdir(frostleafDir, { recursive: true });
+
+    await compileComponentDefs(frostleafDir, componentPaths);
 };
 
 main();
