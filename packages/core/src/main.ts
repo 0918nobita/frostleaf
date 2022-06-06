9fc6a6ffca3cfaf921ce0a60f88ecc3c560bd2f0
0918nobita <nobita.0918@gmail.com>
2022-06-06 12:46:58 +0900

    :sparkles: ADT, match を定義するためのヘルパーを用意した

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,3 +1,35 @@
+type ADTValue<
+    TEnum extends symbol,
+    TDef extends { [_ in keyof TDef]: unknown[] },
+    TVariant extends keyof TDef = keyof TDef
+> = { enum: TEnum; variant: TVariant; params: TDef[TVariant] };
+
+const variant =
+    <TEnum extends symbol, TDef extends { [_ in keyof TDef]: unknown[] }>(
+        e: TEnum
+    ) =>
+    <TVariant extends keyof TDef>(
+        variant: TVariant,
+        ...params: TDef[TVariant]
+    ): ADTValue<TEnum, TDef, TVariant> => ({ enum: e, variant, params });
+
+const match =
+    <TEnum extends symbol, TDef extends { [_ in keyof TDef]: unknown[] }>(
+        e: TEnum
+    ) =>
+    <TVariant extends keyof TDef, TOut>(
+        value: ADTValue<TEnum, TDef, TVariant>,
+        matchers: { [V in keyof TDef]: (...params: TDef[V]) => TOut }
+    ) => {
+        if (value.enum !== e)
+            throw new Error(
+                `Invalid enum (Expected: ${String(e)}, Actual: ${String(
+                    value.enum
+                )})`
+            );
+        return matchers[value.variant](...value.params);
+    };
+
 const foo = Symbol("foo");
 const hoge = Symbol("hoge");
 const fuga = Symbol("fuga");
@@ -7,39 +39,21 @@ type FooADT = {
     [fuga]: [string, number];
 };
 
-/*
-type FooADTValue<TVariant extends keyof FooADT = keyof FooADT> = {
-    enum: typeof foo;
-    variant: TVariant;
-    params: FooADT[TVariant];
-};
-*/
+type FooADTValue<TVariant extends keyof FooADT = keyof FooADT> = ADTValue<
+    typeof foo,
+    FooADT,
+    TVariant
+>;
 
-type ADTValue<
-    TEnum extends symbol,
-    TDef,
-    TVariant extends keyof TDef = keyof TDef
-> = { enum: TEnum; variant: TVariant; params: TDef[TVariant] };
+const fooVariant = variant<typeof foo, FooADT>(foo);
 
-const fooADT = <TVariant extends keyof FooADT>(
-    variant: TVariant,
-    ...params: FooADT[TVariant]
-): ADTValue<typeof foo, FooADT, TVariant> => ({
-    enum: foo,
-    variant,
-    params,
-});
-
-const matchFooADT = <TVariant extends keyof FooADT, TOut>(
-    value: ADTValue<typeof foo, FooADT, TVariant>,
-    matchers: { [V in keyof FooADT]: (...params: FooADT[V]) => TOut }
-): TOut => matchers[value.variant](...value.params);
-
-const hogeVal = fooADT(hoge, 1);
-const fugaVal = fooADT(fuga, "hello", 2);
+const hogeVal = fooVariant(hoge, 1);
+const fugaVal = fooVariant(fuga, "hello", 2);
 console.log({ hogeVal, fugaVal });
 
-const getNum = (fooVal: ADTValue<typeof foo, FooADT>) =>
+const matchFooADT = match<typeof foo, FooADT>(foo);
+
+const getNum = (fooVal: FooADTValue) =>
     matchFooADT(fooVal, { [hoge]: (num) => num, [fuga]: (_, num) => num });
 
 console.log({ hoge: getNum(hogeVal), fuga: getNum(fugaVal) });
