59e5bcf76939dfc4c838f2ea96f0b11f220eaef3
0918nobita <nobita.0918@gmail.com>
2022-06-13 19:17:32 +0900

    Fable.Promise を導入した

--- packages/core/src/App.fs
+++ packages/core/src/App.fs
@@ -1,3 +1,8 @@
 module App
 
-let () = printfn "Hello from F#!"
+let () =
+    promise {
+        return! Promise.create (fun resolve _reject -> resolve "Hello, world!")
+    }
+    |> Promise.map (printfn "%s")
+    |> ignore
